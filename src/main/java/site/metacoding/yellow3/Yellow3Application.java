a1b125a4d2cceb178a412700b10eb283e593a61f
wpem3123 <wpem3123@naver.com>
2022-08-31 10:15:34 +0900

    프로젝트 세팅완료

---
+++ src/main/java/site/metacoding/yellow3/Yellow3Application.java
@@ -0,0 +1,13 @@
+package site.metacoding.yellow3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Yellow3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Yellow3Application.class, args);
+	}
+
+}
---
+++ src/test/java/site/metacoding/yellow3/Yellow3ApplicationTests.java
@@ -0,0 +1,13 @@
+package site.metacoding.yellow3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Yellow3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
